13b6770251c7620e45f01edbf341bf72b5fc59c1
Zonecodez <kalvinjp@gmail.com>
2018-12-09 20:34:25 -0500

    Update skripts.js

--- skripts.js
+++ skripts.js
@@ -8,60 +8,20 @@ window.onload = function() {
     doStuff();
 };
 
-/*
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
+/ toggle about-overlay when it itself is clicked
+var aboutOverlay = document.getElementById('about-overlay');
+aboutOverlay.addEventListener('mousedown', toggleOverlay);
 
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
+// toggle about-overlay when about-btn clicked
+var aboutBtn = document.getElementById('about-btn');
+aboutBtn.addEventListener('mousedown', toggleOverlay);
 
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
+// toggles the display property of the about-overlay between none and block
+function toggleOverlay() {
+  if (window.getComputedStyle(aboutOverlay, null).getPropertyValue('display') == 'none') {
+    aboutOverlay.style.display = 'block';
   }
-
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-  var that = this;
-  var delta = 300 - Math.random() * 100;
-
-  if (this.isDeleting) { delta /= 2; }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
+  else {
+    aboutOverlay.style.display = 'none';
   }
-
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
-
-window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-    }
-  
-  // INJECT CSS
-  var css = document.createElement("styld");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-  document.body.appendChild(css);
-};*/
+}

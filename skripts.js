1f2647b54fc2c95e38a86f2276e2bbb67ba9a1a7
Zonecodez <kalvinjp@gmail.com>
2018-12-09 20:51:43 -0500

    Update skripts.js

--- skripts.js
+++ skripts.js
@@ -8,20 +8,3 @@ window.onload = function() {
     doStuff();
 };
 
-/ toggle about-overlay when it itself is clicked
-var aboutOverlay = document.getElementById('about-overlay');
-aboutOverlay.addEventListener('mousedown', toggleOverlay);
-
-// toggle about-overlay when about-btn clicked
-var aboutBtn = document.getElementById('about-btn');
-aboutBtn.addEventListener('mousedown', toggleOverlay);
-
-// toggles the display property of the about-overlay between none and block
-function toggleOverlay() {
-  if (window.getComputedStyle(aboutOverlay, null).getPropertyValue('display') == 'none') {
-    aboutOverlay.style.display = 'block';
-  }
-  else {
-    aboutOverlay.style.display = 'none';
-  }
-}
